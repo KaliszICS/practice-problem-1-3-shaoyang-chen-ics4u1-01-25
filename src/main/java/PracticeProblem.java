9070ea6a83fe7773d54e04384e7dd06eedf67e7a
Shaoyang Chen <shaoyang.chen@student.tdsb.on.ca>
2025-10-02 14:51:59 -0400

    Update PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,40 @@
+import java.io.FileReader;
+import java.io.IOException;
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
-
+	public static String readFile(String filename){
+		FileReader fr = null;
+		StringBuilder sb = new StringBuilder();
+		try {
+			fr = new FileReader(filename);
+			int character;
+			while ((character = fr.read()) != -1) {
+				sb.append((char) character);
+			}
+		} catch (IOException e) {
+				System.out.println(e);
+				return "";
+		} finally {
+			if (fr != null) {
+				try {
+					fr.close();
+				} catch (IOException e) {
+					System.err.println(e);
+					return "";
+				}
+			}
+		}
+		return sb.toString();
+	}
+	public static String backwardsReadFile(String filename){
+		String normalResult = readFile(filename);
+		String backwardsResult = new StringBuilder(normalResult).reverse().toString();
+		return backwardsResult;
+	}
 	
 
 }
